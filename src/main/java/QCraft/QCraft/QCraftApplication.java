1f10a8e91880eb0b90d95d1aa732129eb32f6ca4
Doyeong Lee <09jjrr21@naver.com>
2024-09-14 05:36:06 +0900

    feat: build.gradle, propertise 설정

--- src/main/java/QCraft/QCraft/QCraftApplication.java
+++ src/main/java/QCraft/QCraft/QCraftApplication.java
@@ -1,13 +1,13 @@
-package QCraft.Qcraft;
+package QCraft.QCraft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QcraftApplication {
+public class QCraftApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(QcraftApplication.class, args);
+		SpringApplication.run(QCraftApplication.class, args);
 	}
 
 }
--- src/test/java/QCraft/QCraft/QcraftApplicationTests.java
+++ src/test/java/QCraft/QCraft/QcraftApplicationTests.java
@@ -1,4 +1,4 @@
-package QCraft.Qcraft;
+package QCraft.QCraft;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
